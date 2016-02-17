518e30ec4f629741c06e10fe1290961238438c96
imrankhan <imran@gmail.com>
2016-02-17 01:13:21 -0600

    Light on off sample  code using arduino

---
+++ light_on_off.ino
@@ -0,0 +1,20 @@
+void setup
+(
+// pin 3 out put
+pinMode(3, OUTPUT);
+
+}
+// loop function
+
+void loop()
+
+{
+
+digitalWrite(3, HIGH); // turn the LED on 
+
+delay(1000); // wait for a second
+
+digitalWrite(3, LOW); // turn the LED off 
+delay(1000); // wait for a second
+
+}
